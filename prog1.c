5c292c7c0a000a6b801925d40b1be07f62bccd74
remoramu12 <53562477+remoramu12@users.noreply.github.com>
2020-02-24 11:13:42 +0530

    Create prog1.c

---
+++ prog1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+int year;
+printf("\n Enter the year:");
+scanf("%d",&year);
+if((year%4==0 && year%100!=0)||(year%400 ==0))
+{
+	printf("\n the year %d is a leap year",year);
+}
+else
+{
+	printf("the year %d is not a leap year",year);
+}
+return 0;
+}
